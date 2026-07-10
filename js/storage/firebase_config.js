1a7dd4e6ff1901b68d3a1e5610c0ade4dad676b8
Cliff-Lee <38708995+Cliff-Lee@users.noreply.github.com>
2026-07-10 12:36:51 +0800

    Add files via upload

---
+++ js/storage/firebase_config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBQyd8w_2FYwYpChypEeKP5BzsFszog6vg",
+  authDomain: "dpaa-2a085.firebaseapp.com",
+  projectId: "dpaa-2a085",
+  storageBucket: "dpaa-2a085.firebasestorage.app",
+  messagingSenderId: "738092319259",
+  appId: "1:738092319259:web:480edfc5e04ee8d9f02ebe"
+};
